08d56bb44b58f76316aa89788a11038942b89b64
tony <serratedhumor@yahoo.com>
2019-11-13 15:00:06 -0600

    mvp met

---
+++ team-builder/src/components/Data.js
@@ -0,0 +1,23 @@
+let data = [
+    {
+        ID: 1,
+        Name: 'Nick',
+        Email: 'x',
+        Role: 'Team Lead'
+
+    },
+    {
+        ID: 2,
+        Name: 'Tony',
+        Email: 'x',
+        Role: 'Full Stack'
+    },
+    {
+        ID: 3,
+        Name: 'Ryan',
+        Email: 'x',
+        Role: 'Full Stack'
+    }
+]
+
+export default data
